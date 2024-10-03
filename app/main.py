cb5f7f551f7e5916e009cfe1686421d73a045831
Benjamin Pogacar <bpogacar@bu.edu>
2024-10-02 20:26:17 -0400

    added pages

--- app/main.py
+++ app/main.py
@@ -1,17 +1,34 @@
 import tkinter as tk
+from home import HomePage
 
 root = tk.Tk()
 
 root.title("Secret Santa Application")
 root.geometry("1000x750")
 
-label = tk.Label(root, text="Click to find out who is getting gifts for who in this years secret santa!")
-label.pack(pady=10)
 
-def on_randomize_click():
-    label.config(text="Here are the pairings!")
+def change_frame(frame):
+    frame.tkraise()
 
-randomizeButton = tk.Button(root, text="Generate List", command=on_randomize_click)
-randomizeButton.pack(pady=10)
+menu_bar = tk.Menu(root)
+
+pages_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Pages", menu=pages_menu)
+
+container = tk.Frame(root)
+container.pack(fill="both", expand=True)
+container.grid_columnconfigure(0, weight=1)
+container.grid_rowconfigure(0, weight=1)
+
+homepage = HomePage(container)
+namelist_page = tk.Frame(container, bg="lightpink")
+
+pages_menu.add_command(label="Home Page", command=lambda: change_frame(homepage))
+pages_menu.add_command(label="Edit Namelist", command=lambda: change_frame(namelist_page))
+pages_menu.add_command(label="Exit", command=root.quit)
+
+root.config(menu=menu_bar)
+
+change_frame(homepage)
 
 root.mainloop()
