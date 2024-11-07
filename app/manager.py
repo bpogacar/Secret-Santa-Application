ff651f44b2dff8ccc324095f566d1ef2bd61b6cd
Benjamin Pogacar <bpogacar@bu.edu>
2024-11-07 14:09:41 -0500

    added returning None feature and remove_associated_constraints() method

--- app/manager.py
+++ app/manager.py
@@ -9,26 +9,47 @@ import random
 class NameListManager:
     def __init__(self, nameList=set(), constraints=set()):
         self.names = nameList
-        self.constraints = constraints  # set of tuples where the first name cannot be paired with the second
+        self.constraints = constraints # set of tuples: (giver, receiver)
         for name in nameList:
+            # ensure people cannot gift themselves
             self.add_constraint(name, name)
     
     def add_name(self, name):
-        # be sure to include logic rules: a person cannot gift themselves
+        if name in self.names:
+            return None
         self.names.add(name)
         self.add_constraint(name, name)
+        return name
     
-    def remove_name(self, name):  # remember to remove any associated constraints
-        # or have a pop up for removing names with existing constraints
+    def remove_name(self, name):
         if self.names and name in self.names:
             self.names.remove(name)
+            return name
+        return None
+
+    def remove_associated_constraints(self, name):
+        # we dont auto-remove the constraints when you delete a name
+        # but we will give the user the option to if they want
+        if not self.constraints:
+            return None
+        constraints = list(self.constraints)
+        for constraint in constraints:
+            if constraint[0] == name or constraint[1] == name:
+                self.constraints.remove(constraint)
+        return name
+        
 
     def add_constraint(self, giver, receiver):
+        if giver not in self.names or receiver not in self.names or (giver, receiver) in self.constraints:
+            return None
         self.constraints.add((giver, receiver))
+        return (giver, receiver)
     
     def remove_constraint(self, giver, receiver):
         if self.constraints and (giver, receiver) in self.constraints:
             self.constraints.remove((giver, receiver))
+            return (giver, receiver)
+        return None
     
     def generate_pairs(self):
         # use backtracking to generate all possible pairings, stopping when you get
